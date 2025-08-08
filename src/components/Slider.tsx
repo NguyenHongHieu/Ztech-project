f6582f1d8f2bb91daf245a7bca138ef1b87f96bc
NguyenHongHieu <nguyenhonghieuit@gmail.com>
2025-08-08 13:45:43 +0700

    fix image in slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Slider() {
   return (
-    <div className="w-full h-[105px] sm:h-[415px] bg-gray-200 flex items-center justify-center rounded-lg overflow-hidden">
+    <div className="w-full h-[130px] sm:h-[415px] bg-gray-200 flex items-center justify-center rounded-lg overflow-hidden">
       <div className="relative w-full h-full">
-        <Image src="/slider.svg" alt="Slider" fill className="object-contain sm:object-cover" sizes="(max-width: 640px) 100vw, 100vw" />
+        <Image src="/slider.svg" alt="Slider" fill className="object-cover" sizes="(max-width: 640px) 100vw, 100vw" />
       </div>
     </div>
   );
